9af961104195c9717695f966221e303cbb83847e
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-02-22 02:02:49 -0600

    added class implementation

---
+++ classes/classes.js
@@ -0,0 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + ". " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+var new_user = new Student("BRYCE", "A", "Soghigian");
+document.body.textContent = greeter(new_user);
